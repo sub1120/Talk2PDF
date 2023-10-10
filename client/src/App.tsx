1484ff7793da6d1b1dccf4f58b21602bda16df16
sub1120 <subhamsahu1120@gmail.com>
2023-10-10 11:57:17 +0530

    fix:updated dependencies and minor UI fixes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -136,85 +136,94 @@ function App() {
   };
 
   return (
-    <main className="m-10  md:m-15 lg:m-20 space-y-5">
-      <ToastContainer
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-      {/* heading */}
-      <section className="text-center">
-        <h1 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-gray-800">
-          🎙Talk2PDF🎤
-        </h1>
-        <h2 className="text-md md:text-lg lg:text-xl mt-5 text-gray-800">
-          Now Ask any question to your PDF.
-        </h2>
-      </section>
+    <main className="p-10 flex items-center md:p-15 lg:p-20 space-y-5 h-[100vh]">
+      <div className="w-full">
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+        {/* heading */}
+        <section className="text-center">
+          <h1 className="text-4xl md:text-3xl lg:text-4xl font-semibold text-gray-800">
+            🎙Talk2PDF🎤
+          </h1>
+          <h2 className="text-md md:text-lg lg:text-xl mt-5 text-gray-800">
+            Now Ask any question to your PDF.
+          </h2>
+        </section>
 
-      {/* file input */}
-      <section className="space-y-2">
-        {files.length === 0 && (
-          <>
-            <h2 className="text-md md:text-lg lg:text-xl">Upload PDFs</h2>
-            <FileInput
-              submitHandler={fileSubmitHandler}
-              disabled={loading.isUploading}
-              isUploading={loading.isUploading}
-            />
-          </>
-        )}
-        {files && (
-          <div className="flex justify-between">
-            <span>{`${files.length} files uploaded `} </span>
-            <button
-              className="text-white bg-orange-500 w-24 hover:bg-orange-600 p-2"
-              onClick={resetHandler}
-              disabled={loading.isDeleting}
-            >
-              Reset
-            </button>
-          </div>
-        )}
-      </section>
-
-      {/* chat box */}
-      {files.length !== 0 && (
-        <section className="bg-gray-100 rounded-md h-52 relative">
-          {chat.question && (
+        {/* file input */}
+        <section className="space-y-2">
+          {files.length === 0 && (
             <>
-              <div className="bg-gray-100  mt-0">
-                <p className="text-left py-3 px-10 text-gray-800">
-                  <span className="font-semibold">You: </span>
-                  {chat.question}
-                </p>
-              </div>
-              <div className=" bg-gray-300 flex mt-0">
-                <p className="text-left px-10 py-3 text-gray-800">
-                  <span className="font-semibold">PDF:</span>{" "}
-                  {loading.isRetriving ? "🚀 Loading..." : chat.answer}
-                </p>
-              </div>
+              <h2 className="text-md md:text-lg lg:text-xl">Upload PDFs</h2>
+              <FileInput
+                submitHandler={fileSubmitHandler}
+                disabled={loading.isUploading}
+                isUploading={loading.isUploading}
+              />
             </>
           )}
-
-          <div className="absolute bottom-0 py-2 px-2 w-full">
-            <ChatInput
-              disabled={loading.isRetriving}
-              value={currentQuestion}
-              submitHandler={questionSubmitHandler}
-              onChangeHandler={onChangeHandler}
-            />
-          </div>
+          {files && (
+            <div className="flex justify-between mb-2">
+              <span>{`${files.length} files uploaded `} </span>
+              <button
+                className="text-white bg-orange-500 w-24 hover:bg-orange-600 p-2"
+                onClick={resetHandler}
+                disabled={loading.isDeleting}
+              >
+                Reset
+              </button>
+            </div>
+          )}
         </section>
-      )}
+
+        {/* chat box */}
+        {files.length !== 0 && (
+          <section className="bg-gray-100 rounded-md h-52 relative">
+            {chat.question ? (
+              <>
+                <div className="bg-gray-100  mt-0">
+                  <p className="text-left py-3 px-10 text-gray-800">
+                    <span className="font-semibold">You: </span>
+                    {chat.question}
+                  </p>
+                </div>
+                <div className=" bg-gray-300 flex mt-0">
+                  <p className="text-left px-10 py-3 text-gray-800">
+                    <span className="font-semibold">PDF:</span>{" "}
+                    {loading.isRetriving
+                      ? "🚀 Loading..."
+                      : chat.answer ||
+                        "Apology, Could you please provide more context!"}
+                  </p>
+                </div>
+              </>
+            ) : (
+              <div className="flex items-center justify-center h-full text-gray-500">
+                No Chat Found
+              </div>
+            )}
+
+            <div className="absolute bottom-0 py-2 px-2 w-full">
+              <ChatInput
+                disabled={loading.isRetriving}
+                value={currentQuestion}
+                submitHandler={questionSubmitHandler}
+                onChangeHandler={onChangeHandler}
+              />
+            </div>
+          </section>
+        )}
+      </div>
     </main>
   );
 }
