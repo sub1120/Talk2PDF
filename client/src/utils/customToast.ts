a8a2c4b7785ecf01ff4c95efb655c80d17e58a89
sub1120 <subhamsahu1120@gmail.com>
2023-10-10 13:05:15 +0530

    fix: fixed toaster

--- client/src/utils/customToast.ts
+++ client/src/utils/customToast.ts
@@ -3,31 +3,29 @@ import { toast } from "react-toastify";
 const notify = (type: string, message: string) => {
   switch (type) {
     case "success":
-      () =>
-        toast.success(message, {
-          position: "top-center",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-        });
+      toast.success(message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
       break;
 
     case "error":
-      () =>
-        toast.error(message, {
-          position: "top-center",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-        });
+      toast.error(message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
       break;
   }
 };
